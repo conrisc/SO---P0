cdc102cc13a6f102a7761e1c7655d88342fce32c
Konrad Cielecki <conrisc@gmail.com>
2016-10-30 15:29:33 +0100

    That's almost everything

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <pthread.h>
 
+#define NUM_THREADS 13
+
+void *sayHello(void *threadID) {
+  printf("Hello Wordl! Thread nr %ld\n",(long)threadID);
+  pthread_exit(NULL);
+}
 
 int main() {
 
-  printf("Test\n");
+  pthread_t threads[NUM_THREADS];
 
+  for (long i=0;i<NUM_THREADS;i++) {
+    pthread_create(&threads[i], NULL, sayHello, (void *)i);
+  }
+  printf("Hello World! I'm main thread.\n");
+  pthread_exit(NULL);
+  printf("LOL\n");
   return 0;
 }
